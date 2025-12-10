582f4bd208dea7a7a24e8efb78f1da8cb9046d36
wbc3467-spec <wbc3467@gmail.com>
2025-12-10 10:39:16 +0000

    cleanup: barParticle cleanup

--- src/main/java/net/torocraft/torohealth/bars/BarStates.java
+++ src/main/java/net/torocraft/torohealth/bars/BarStates.java
@@ -1,8 +1,6 @@
 package net.torocraft.torohealth.bars;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
@@ -11,7 +9,6 @@ import net.minecraft.entity.LivingEntity;
 public class BarStates {
 
   private static final Map<Integer, BarState> STATES = new HashMap<>();
-  public static List<BarParticle> PARTICLES = new ArrayList<>();
   private static int tickCount = 0;
 
   public static BarState getState(LivingEntity entity) {
@@ -32,9 +29,6 @@ public class BarStates {
     if (tickCount % 200 == 0) {
       cleanCache();
     }
-
-    PARTICLES.forEach(p -> p.tick());
-    PARTICLES.removeIf(p -> p.age > 50);
 
     tickCount++;
   }

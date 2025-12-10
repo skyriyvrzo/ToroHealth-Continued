c241106d16828de76c58d1e8e9f98be8dc42462a
wbc3467-spec <wbc3467@gmail.com>
2025-12-10 10:39:16 +0000

    cleanup: barParticle cleanup

--- src/main/java/net/torocraft/torohealth/mixin/WorldRendererMixin.java
+++ src/main/java/net/torocraft/torohealth/mixin/WorldRendererMixin.java
@@ -11,7 +11,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.Matrix4f;
 import net.torocraft.torohealth.bars.HealthBarRenderer;
-import net.torocraft.torohealth.bars.ParticleRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -37,7 +36,6 @@ public class WorldRendererMixin {
       boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer,
       LightmapTextureManager lightmapTextureManager, Matrix4f matrix, CallbackInfo info) {
     HealthBarRenderer.renderInWorld(matrices, camera);
-    ParticleRenderer.renderParticles(matrices, camera);
   }
 
 }

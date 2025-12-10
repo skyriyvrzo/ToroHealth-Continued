6c3139f50a594991f7902d45b63898f80598a90d
wbc3467-spec <wbc3467@gmail.com>
2025-12-10 09:51:57 +0000

    Refactor: Re-Implementated health change particles using minecraft particle class, changed mod initialization to ClientModInitializer

--- src/main/java/net/torocraft/torohealth/ToroHealth.java
+++ src/main/java/net/torocraft/torohealth/ToroHealth.java
@@ -1,13 +1,20 @@
 package net.torocraft.torohealth;
 
 import java.util.Random;
-import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.torocraft.torohealth.config.Config;
 import net.torocraft.torohealth.config.loader.ConfigLoader;
 import net.torocraft.torohealth.display.Hud;
 import net.torocraft.torohealth.util.RayTrace;
+import net.torocraft.torohealth.particle.HealthChangeParticle;
+import net.fabricmc.api.ClientModInitializer;
 
-public class ToroHealth implements ModInitializer {
+public class ToroHealth implements ClientModInitializer {
 
   public static final String MODID = "torohealth";
 
@@ -21,8 +28,20 @@ public class ToroHealth implements ModInitializer {
   private static ConfigLoader<Config> CONFIG_LOADER = new ConfigLoader<>(new Config(),
       ToroHealth.MODID + ".json", config -> ToroHealth.CONFIG = config);
 
+  public static final DefaultParticleType HEALTH_CHANGE = FabricParticleTypes.simple();
+
   @Override
-  public void onInitialize() {
-    CONFIG_LOADER.load();
+  public void onInitializeClient() {
+      CONFIG_LOADER.load();
+      Registry.register(
+          Registry.PARTICLE_TYPE,
+          new Identifier(MODID, "health_change"),
+          HEALTH_CHANGE
+      );
+
+      ParticleFactoryRegistry.getInstance().register(
+          ToroHealth.HEALTH_CHANGE,
+          HealthChangeParticle.HealthChangeFactory::new
+      );
   }
 }
--- src/main/java/net/torocraft/torohealth/bars/BarState.java
+++ src/main/java/net/torocraft/torohealth/bars/BarState.java
@@ -2,6 +2,7 @@ package net.torocraft.torohealth.bars;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.torocraft.torohealth.ToroHealth;
 
 public class BarState {
@@ -62,8 +63,10 @@ public class BarState {
 
     lastDmgDelay = HEALTH_INDICATOR_DELAY * 2;
     lastHealth = health;
-    if (ToroHealth.CONFIG.particle.show) {
-      BarStates.PARTICLES.add(new BarParticle(entity, lastDmg));
+    if (ToroHealth.CONFIG.particle.show && lastDmg != 0) {
+      //BarStates.PARTICLES.add(new BarParticle(entity, lastDmg));
+        Vec3d entityLocation = entity.getPos().add(0, entity.getHeight() / 2, 0);
+        entity.world.addImportantParticle(ToroHealth.HEALTH_CHANGE, true, entityLocation.x, entityLocation.y, entityLocation.z, Double.longBitsToDouble(-lastDmg & 0xFFFFFFFFL), 0, 0);
     }
   }
 

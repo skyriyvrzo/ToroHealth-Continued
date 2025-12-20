8f8241226942bbf8d21d664de5c8441187d956e4
wbc3467-spec <wbc3467@gmail.com>
2025-12-20 16:19:12 +0000

    Update: updated gradle related code to 1.19

--- src/main/java/net/kairost/torohealth/ToroHealth.java
+++ src/main/java/net/kairost/torohealth/ToroHealth.java
@@ -1,6 +1,6 @@
 package net.kairost.torohealth;
 
-import blue.endless.jankson.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.ActionResult;

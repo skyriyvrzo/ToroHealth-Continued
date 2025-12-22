14de6d450f661cac148763cd404677070b94a530
wbc3467-spec <wbc3467@gmail.com>
2025-12-22 05:54:59 +0000

    Fix: fixed Ghast rendering position

--- src/main/java/net/kairost/torohealth/client/gui/ToroHealthHud.java
+++ src/main/java/net/kairost/torohealth/client/gui/ToroHealthHud.java
@@ -172,7 +172,7 @@ public class ToroHealthHud {
             this.entityY = (float) FRAME_SIZE / 2 + ENTITY_RENDER_HEIGHT / 2;
         }
         if (this.entity instanceof GhastEntity) {
-            this.entityY = 3;
+            this.entityY = (float) FRAME_SIZE / 2 + entity.getHeight() * entityScale / 4 ;
         }
         else if (this.entity instanceof EnderDragonEntity) {
             this.entityY = (float) FRAME_SIZE / 2 + entity.getHeight() * this.entityScale / 4;

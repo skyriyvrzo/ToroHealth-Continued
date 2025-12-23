3470a08eb55a945e498945a20c40890fcb598c7c
wbc3467-spec <wbc3467@gmail.com>
2025-12-23 10:12:43 +0000

    Cleanup

--- src/main/java/net/kairost/torohealth/client/particle/HealthChangeParticle.java
+++ src/main/java/net/kairost/torohealth/client/particle/HealthChangeParticle.java
@@ -4,7 +4,6 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.particle.SimpleParticleType;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.client.particle.Particle;
@@ -95,11 +94,6 @@ public class HealthChangeParticle
         float x = (float)(MathHelper.lerp(tickDelta, this.prevPosX, this.x) - vec3d.getX());
         float y = (float)(MathHelper.lerp(tickDelta, this.prevPosY, this.y) - vec3d.getY());
         float z = (float)(MathHelper.lerp(tickDelta, this.prevPosZ, this.z) - vec3d.getZ());
-
-        MatrixStack matrices = new MatrixStack();
-        matrices.translate(x, y, z);
-        matrices.multiply(camera.getRotation());
-        matrices.scale(-0.025f, -0.025f, 0.025f);
 
         String text = Integer.toString(this.value);
         float h = -client.textRenderer.getWidth(text) / 2.0f;

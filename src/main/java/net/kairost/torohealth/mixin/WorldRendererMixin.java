e2fe7ed8c34d801958d4d878d288c494071cdcf1
wbc3467-spec <wbc3467@gmail.com>
2025-12-30 19:59:59 +0000

    Refine: Avoid rendering in-world bar for entities have passenger

--- src/main/java/net/kairost/torohealth/mixin/WorldRendererMixin.java
+++ src/main/java/net/kairost/torohealth/mixin/WorldRendererMixin.java
@@ -26,8 +26,10 @@ public class WorldRendererMixin {
     private void torohealth$renderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta,
         MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo info) {
         if (ToroHealth.getConfig().enabled && !ToroHealth.getConfig().inWorldBarOptions.inWorldBarVisibilityMode.equals(ModConfig.InWorldBarVisibilityMode.NONE)) {
-            int light = ModConfig.INSTANCE.inWorldBarOptions.inWorldBarLightMode.equals(ModConfig.InWorldBarLightMode.FULL_BRIGHT) ? LightmapTextureManager.MAX_LIGHT_COORDINATE : this.entityRenderDispatcher.getLight(entity, tickDelta);
-            InWorldBarRenderer.render(entity, cameraX, cameraY, cameraZ, tickDelta, matrices, vertexConsumers, light, this.entityRenderDispatcher);
+            if (!entity.hasPassengers()) {
+                int light = ModConfig.INSTANCE.inWorldBarOptions.inWorldBarLightMode.equals(ModConfig.InWorldBarLightMode.FULL_BRIGHT) ? LightmapTextureManager.MAX_LIGHT_COORDINATE : this.entityRenderDispatcher.getLight(entity, tickDelta);
+                InWorldBarRenderer.render(entity, cameraX, cameraY, cameraZ, tickDelta, matrices, vertexConsumers, light, this.entityRenderDispatcher);
+            }
         }
     }
 }

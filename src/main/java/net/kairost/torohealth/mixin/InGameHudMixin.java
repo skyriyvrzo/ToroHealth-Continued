ba1da6b0da12c9dc766b99a9bd0c633b20f000c6
wbc3467-spec <wbc3467@gmail.com>
2025-12-31 22:02:55 +0000

    Refine: move Torohealth HUD layer above SpyGlass layer

--- src/main/java/net/kairost/torohealth/mixin/InGameHudMixin.java
+++ src/main/java/net/kairost/torohealth/mixin/InGameHudMixin.java
@@ -8,16 +8,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.kairost.torohealth.ToroHealth;
-
-@Mixin(InGameHud.class)
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo; @Mixin(InGameHud.class)
 public class InGameHudMixin {
     @Final
     @Shadow
     private LayeredDrawer layeredDrawer;
 
-    @Inject(method = "<init>(Lnet/minecraft/client/MinecraftClient;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/LayeredDrawer;addLayer(Lnet/minecraft/client/gui/LayeredDrawer$Layer;)Lnet/minecraft/client/gui/LayeredDrawer;", ordinal = 1))
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/LayeredDrawer;addSubDrawer(Lnet/minecraft/client/gui/LayeredDrawer;Ljava/util/function/BooleanSupplier;)Lnet/minecraft/client/gui/LayeredDrawer;", ordinal = 0, shift = At.Shift.AFTER))
     private void torohealth$initInstance(MinecraftClient client, CallbackInfo info) {
         this.layeredDrawer.addLayer((context, tickDelta) -> ToroHealth.toroHealthHud.render(context, tickDelta));
     }

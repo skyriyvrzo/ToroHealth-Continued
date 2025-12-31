e21a5270d855e407405b19d8c23864e37ed16edc
wbc3467-spec <wbc3467@gmail.com>
2025-12-31 14:21:58 +0000

    Fix: dont render torohealth Hud when set not render hud

--- src/main/java/net/kairost/torohealth/client/gui/ToroHealthHud.java
+++ src/main/java/net/kairost/torohealth/client/gui/ToroHealthHud.java
@@ -60,6 +60,9 @@ public class ToroHealthHud {
         if (!ToroHealth.getConfig().enabled || !ToroHealth.getConfig().hudOptions.showHUD) {
             return;
         }
+        if (this.client.options.hudHidden) {
+            return;
+        }
         if (entity == null) {
             return;
         }

e7c4a49c5858780d4319da7fba0adac9ae88d536
wbc3467-spec <wbc3467@gmail.com>
2026-01-04 14:02:07 +0000

    Refine: minor code refine

--- src/main/java/net/kairost/torohealth/client/util/EntityUtil.java
+++ src/main/java/net/kairost/torohealth/client/util/EntityUtil.java
@@ -39,6 +39,7 @@ public class EntityUtil {
                 || entity instanceof CreeperEntity && ((CreeperEntity) entity).shouldRenderOverlay() // charged creeper
                 || StreamSupport.stream(entity.getItemsEquipped().spliterator(), false).anyMatch(is -> !is.isEmpty()))
             && entity != client.player
+            && !entity.hasPassengers()
             && !entity.isSpectator();
     }
 

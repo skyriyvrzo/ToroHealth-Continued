7171e6ee33c578b195a1cc9fd003e3b45e9d2c1c
wbc3467-spec <wbc3467@gmail.com>
2026-01-12 09:17:40 +0000

    Fix: max particle distance logic, Cleanup

--- src/main/java/net/kairost/torohealth/ToroHealth.java
+++ src/main/java/net/kairost/torohealth/ToroHealth.java
@@ -39,10 +39,6 @@ public class ToroHealth implements ClientModInitializer {
             return ActionResult.SUCCESS;
         });
 
-        holder.registerLoadListener((h, c) -> {
-            c.postLoad();
-            return ActionResult.SUCCESS;
-        });
         config = ModConfig.INSTANCE;
 
         //toroHealth Particle

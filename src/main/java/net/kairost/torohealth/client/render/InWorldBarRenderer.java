f5fa9f80dc86abdd651096adab08ec8f8bd12639
wbc3467-spec <wbc3467@gmail.com>
2026-01-23 09:18:09 +0000

    Refine: Add handler for shulker invisible, code refines

--- src/main/java/net/kairost/torohealth/client/render/InWorldBarRenderer.java
+++ src/main/java/net/kairost/torohealth/client/render/InWorldBarRenderer.java
@@ -114,6 +114,6 @@ public class InWorldBarRenderer {
         if (ToroHealth.getConfig().inWorldBarOptions.onlyWhenLookingAt && ToroHealth.getTargetedEntity() != entity) {
             return false;
         }
-        return EntityUtil.showHealthBar(entity, MinecraftClient.getInstance());
+        return EntityUtil.showHealthBar(entity, MinecraftClient.getInstance().player);
     }
 }

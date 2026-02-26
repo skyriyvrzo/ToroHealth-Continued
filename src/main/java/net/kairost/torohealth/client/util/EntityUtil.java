83aeed0ca657047dacd7d47552e78781a749d6df
wbc3467-spec <wbc3467@gmail.com>
2026-02-26 16:25:48 +0000

    BugFix: removed an unsafe type cast

--- src/main/java/net/kairost/torohealth/client/util/EntityUtil.java
+++ src/main/java/net/kairost/torohealth/client/util/EntityUtil.java
@@ -43,8 +43,8 @@ public class EntityUtil {
         return (!entity.isInvisibleTo(player)
             || entity.isGlowing()
             || entity.isOnFire()
-            || entity instanceof CreeperEntity && ((CreeperEntity) entity).isCharged() // charged creeper
-            || (((LivingEntity) entity).getArmorVisibility() > 0))
+            || (entity instanceof CreeperEntity && ((CreeperEntity) entity).isCharged()) // charged creeper
+            || (entity instanceof LivingEntity && (((LivingEntity) entity).getArmorVisibility() > 0)))
             && !entity.isSpectator();
     }
 
